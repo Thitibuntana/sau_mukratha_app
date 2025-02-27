fd9c3cccb9658fec940576ec82bf767248af39c5
Thitibuntana T <s6652410022@sau.ac.th>
2025-02-27 14:36:08 +0700

    CalBillUI First Step Commit

--- lib/views/cal_bill_ui.dart
+++ lib/views/cal_bill_ui.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import "package:image_picker/image_picker.dart";
+import "package:flutter/services.dart";
 
 class CalBillUI extends StatefulWidget {
   const CalBillUI({super.key});
@@ -8,25 +11,195 @@ class CalBillUI extends StatefulWidget {
 }
 
 class _CalBillUIState extends State<CalBillUI> {
+  bool adultCheck = false;
+  bool childCheck = false;
+
+  TextEditingController adultCtrl = TextEditingController(text: '0');
+  TextEditingController childCtrl = TextEditingController(text: '0');
+
+  File? imgFile;
+  Future<void> openCamera() async {
+    final picker = await ImagePicker().pickImage(
+      source: ImageSource.camera,
+      imageQuality: 75,
+    );
+    if (picker == null) return;
+
+    setState(() {
+      imgFile = File(picker.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
           padding: EdgeInsets.only(
-            left: 8.0,
-            right: 8.0,
+            left: 35.0,
+            right: 35.0,
           ),
           child: Center(
-            child: Column(children: [
-              SizedBox(
-                height: 50,
-              ),
-              Image.asset(
-                'assets/images/camera.jpg',
-                height: 120.0,
-              ),
-            ]),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 50,
+                ),
+                InkWell(
+                  onTap: () {
+                    openCamera();
+                  },
+                  child: imgFile == null
+                      ? Image.asset(
+                          'assets/images/camera.jpg',
+                          height: 120.0,
+                        )
+                      : Image.file(
+                          imgFile!,
+                          height: 120.0,
+                        ),
+                ),
+                SizedBox(
+                  height: MediaQuery.sizeOf(context).height * 0.05,
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Amount of people',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.sizeOf(context).height * 0.01,
+                ),
+                Row(
+                  children: [
+                    Checkbox(
+                      onChanged: (v) {
+                        setState(() {
+                          adultCheck = v!;
+                          if (v! == false) {
+                            adultCtrl.text = '0';
+                          }
+                        });
+                      },
+                      value: adultCheck,
+                    ),
+                    SizedBox(
+                      width: 165,
+                      child: Text(
+                        'Adult - 299฿ / person: ',
+                        style: TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 25,
+                    ),
+                    Flexible(
+                      child: TextField(
+                        controller: adultCtrl,
+                        enabled: adultCheck,
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly
+                        ],
+                        decoration: InputDecoration(
+                          suffix: Text(
+                            ' People',
+                            style: TextStyle(
+                              fontSize: 12,
+                            ),
+                          ),
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: MediaQuery.sizeOf(context).height * 0.01,
+                ),
+                Row(
+                  children: [
+                    Checkbox(
+                      onChanged: (v) {
+                        setState(() {
+                          childCheck = v!;
+                          if (v! == false) {
+                            childCtrl.text = '0';
+                          }
+                        });
+                      },
+                      value: childCheck,
+                    ),
+                    SizedBox(
+                      width: 165,
+                      child: Text(
+                        'Child - 69฿ / person: ',
+                        style: TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 25,
+                    ),
+                    Flexible(
+                      child: TextField(
+                        controller: childCtrl,
+                        enabled: childCheck,
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly
+                        ],
+                        decoration: InputDecoration(
+                          suffix: Text(
+                            ' People',
+                            style: TextStyle(
+                              fontSize: 12,
+                            ),
+                          ),
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: MediaQuery.sizeOf(context).height * 0.015,
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Buffet Drinks',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                 SizedBox(
+                  height: MediaQuery.sizeOf(context).height * 0.025,
+                ),
+                Row(
+                  children: [
+                    Radio(
+                      onChanged: (v) {},
+                      value: 1,
+                      groupValue: null,
+                    ),
+                    Text(
+                      '25฿ / Person'
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
